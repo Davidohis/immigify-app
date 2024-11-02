0b1ab2016a4873df4bca6b024483558aeb8c4603
Davidohis <davidi.ohis25@gmail.com>
2024-11-02 12:11:17 +0100

    worked on mobile responsiveness and ui fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import EnvironmentPlugin from "vite-plugin-environment";
 import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import postcss from "postcss";
@@ -10,7 +9,6 @@ export default defineConfig({
   plugins: [
     react(),
     postcss(),
-    EnvironmentPlugin(["VITE_REACT_APP_API_URL"]),
     AutoImport({
       dirs: ["./src/hooks"],
       dts: true,
